7bbddabb75d16f8fdd28655ff1138ce80e20be31
yuri <913463115@qq.com>
2017-02-17 14:43:15 +0800

    demo1

--- demo1/reducers/index.js
+++ demo1/reducers/index.js
@@ -1,8 +1,8 @@
-
-import { combineReducers } from 'redux'
+import {combineReducers} from 'redux'
 import counter from './counter'
-//使用redux的combineReducers方法将所有reducer打包起来
+
 const rootReducer = combineReducers({
-  counter
+    counter
 })
+
 export default rootReducer
